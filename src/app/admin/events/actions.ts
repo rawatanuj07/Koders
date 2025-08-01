8356346c687a79a54e4a29ac22e93349b1d73134
rawatanuj07 <rawatanuj07@gmail.com>
2025-08-02 00:27:02 +0530

    events-vercelerror-fixed

---
+++ src/app/admin/events/actions.ts
@@ -0,0 +1,8 @@
+import clientPromise from "@/app/libs/mongodb";
+export async function deleteEventById(eventId: string) {
+  const client = await clientPromise;
+  const db = client.db("koders");
+  const eventsCollection = db.collection("events");
+
+  await eventsCollection.deleteOne({ id: eventId });
+}
--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,7 +1,8 @@
 "use server";
 import { getEvents } from "@/app/libs/db/events";
 import AdminEventsPageClient from "@/components/AdminEventsPageClient";
-import clientPromise from "@/app/libs/mongodb";
+import { deleteEventById } from "@/app/admin/events/actions";
+
 export default async function AdminEventsPage() {
   const events = await getEvents(); // safe server-side DB call
   return (
@@ -10,12 +11,4 @@ export default async function AdminEventsPage() {
       deleteEvent={deleteEventById}
     />
   );
-}
-
-export async function deleteEventById(eventId: string) {
-  const client = await clientPromise;
-  const db = client.db("koders");
-  const eventsCollection = db.collection("events");
-
-  await eventsCollection.deleteOne({ id: eventId });
 }
